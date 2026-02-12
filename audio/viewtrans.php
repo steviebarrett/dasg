6bdd27d00bfd954e9a5fb27513dca134b7e6d8c0
steviebarrett <mail@steviebarrett.com>
2026-02-12 14:35:48 +0000

    Another fix for path traversal vulnerability with STRICT allowlist in /audio/viewtrans.php

--- audio/viewtrans.php
+++ audio/viewtrans.php
@@ -1,128 +1,147 @@
 <?php
+declare(strict_types=1);
 
 $pageTitle = "Cluas ri Claisneachd";
-$pageSlug = "audio";
-
-$cqpPage = true;
+$pageSlug  = "audio";
+$cqpPage   = true;
 
 $javascriptBlock = <<<HTML
 <script>
-	var elem;
+  var elem;
+  $(function() {
+    var elems = $('span.highlight').toArray();
+    var id = -1;
+    $('#findPrev').addClass('disabled');
 
-	$(function() {
+    $('.findTerm').on('click', function() {
+      $('.highlight').removeClass('highlightStrong');
+      if ($(this).attr('id') == 'findPrev') {
+        id--;
+      } else {
+        id++;
+        $('#findPrev').removeClass('disabled');
+      }
+      if (id >= elems.length-1) {
+        id = elems.length-1;
+        $('#findNext').addClass('disabled');
+      } else if (id < 1) {
+        id = 0;
+        $('#findPrev').addClass('disabled');
+        $('#findNext').removeClass('disabled');
+      } else {
+        $('#findNext').removeClass('disabled');
+      }
+      elem = elems[id];
+      $(elem).addClass('highlightStrong');
+      var idPos = $(elem).offset().top;
+      $('html,body').animate({scrollTop: idPos-150},'fast');
+    });
 
-		var elems = $('span.highlight').toArray();
-		var id = -1;
-		$('#findPrev').addClass('disabled');
-
-		$('.findTerm').on('click', function() {
-			$('.highlight').removeClass('highlightStrong');
-			if ($(this).attr('id') == 'findPrev') {
-				id--;
-			} else {
-				id++;
-				$('#findPrev').removeClass('disabled');
-			}
-			if (id >= elems.length-1) {
-				id = elems.length-1;
-				$('#findNext').addClass('disabled');
-			} else if (id < 1) {
-				id = 0;	
-				$('#findPrev').addClass('disabled');
-				$('#findNext').removeClass('disabled');
-			} else {
-				$('#findNext').removeClass('disabled');
-			}
-			elem = elems[id];
-			$(elem).addClass('highlightStrong');
-			var idPos = $(elem).offset().top;
-            $('html,body').animate({scrollTop: idPos-150},'fast');
-		});
-
-		$('#transTop').on('click', function() {
-			$('html,body').animate({scrollTop: 0},'fast');
-			id = -1;
-			$('#findPrev').addClass('disabled');
-			$('#findNext').removeClass('disabled');
-		});
-	});
+    $('#transTop').on('click', function() {
+      $('html,body').animate({scrollTop: 0},'fast');
+      id = -1;
+      $('#findPrev').addClass('disabled');
+      $('#findNext').removeClass('disabled');
+    });
+  });
 </script>
 HTML;
 
 require_once '../includes/include.php';
 require_once '../includes/htmlHeader.php';
 
-$q = Functions::e(["q"]);
-echo <<<HTML
-	<div class="fixedNav">
-		<h4 class="transEx">{$q}</h4>
-		<div class="controlBar">
-			<input id="findPrev" class="findTerm" value="prev" type="button"/>
-			<input id="findNext" class="findTerm" value="next" type="button"/>
-		</div>
-		<input type="button" id="transTop" value="back to top" type="button"/>
-	</div>
-HTML;
+// 1) Strict allowlist
+$ALLOWED_TRANSCRIPTIONS = [
+    'ABB_Tape2' => true,
+    'ABB_Tape5' => true,
+    'Calum_MacNeil_2' => true,
+    'Calum_MacNeil_3' => true,
+    'Calum_MacNeil_4' => true,
+    'Calum_MacNeil_5' => true,
+    'Calum_MacNeil_6' => true,
+    'Courting_on_Scalpay' => true,
+    'GnE_Agallamh_Alasdair_MacEachainn' => true,
+    'GnE_Agallamh_Caitriona_NicCumhais' => true,
+    'GnE_Agallamh_Domhnall_Fearghasdan' => true,
+    'GnE_Agallamh_Floraidh_NicDhomhnaill' => true,
+    'GnE_Agallamh_Iseabail_Graham' => true,
+    'GnE_Agallamh_Mairi_NicAonghais' => true,
+    'GnE_Agallamh_Mary_Ellen_Stiubhart' => true,
+    'GnE_Agallamh_Niall_Domhnallach' => true,
+    'GnE_Agallamh_Ruairidh_MacIllEathain' => true,
+    'GnE_Agallamh_Seumas_Domhnallach' => true,
+    'GnE_Agallamh_Seumas_MacIllEidich' => true,
+    'GnE_Oran_Seumas_MacIllEidich' => true,
+    'GU_AJ_Smith_01or02' => true,
+    'GU_AJ_Smith_03' => true,
+    'GU_AJ_Smith_04' => true,
+    'GU_AJ_Smith_08' => true,
+    'GU_AJ_Smith_15' => true,
+    'GU_CIN_MacLeod_03' => true,
+    'GU_CIN_MacLeod_28' => true,
+    'GU_Mrs_Marion_Montgomery' => true,
+    'GU_North_Argyll' => true,
+    'GU_Sister_Beatons' => true,
+    'GU_William_Mackay' => true,
+    'Origins_and_Religion_from_Barra' => true,
+    'Stories_and_Songs_from_Berneray' => true,
+    'Stories_from_Gobhaig' => true,
+    'Stories_from_Sollas' => true,
+];
 
-
+// 2) Validate ref
 $ref = $_GET['ref'] ?? '';
-if (!is_string($ref)) {
-    http_response_code(400);
-    exit('Bad request');
-}
-
-/**
- * STRICT allowlist:
- * - No slashes, no dots, no colons => blocks traversal and URL schemes.
- * - Adjust max length to your IDs.
- */
-if (!preg_match('/\A[A-Za-z0-9_-]{1,80}\z/', $ref)) {
+if (!is_string($ref) || !isset($ALLOWED_TRANSCRIPTIONS[$ref])) {
     http_response_code(404);
-    exit('Not found');
+    echo 'Not found';
+    require_once '../includes/htmlFooter.php';
+    exit;
 }
 
-$baseDir = realpath(__DIR__ . '/transcriptions');
-if ($baseDir === false) {
-    http_response_code(500);
-    exit('Server misconfig');
-}
-
-// Build path and resolve it (realpath requires existence)
-$requested = $baseDir . DIRECTORY_SEPARATOR . $ref . '.txt';
-$realFile = realpath($requested);
-if ($realFile === false) {
+// 3) Read file
+$file = __DIR__ . '/transcriptions/' . $ref . '.txt';
+if (!is_file($file) || !is_readable($file)) {
     http_response_code(404);
-    exit('Not found');
+    echo 'Not found';
+    require_once '../includes/htmlFooter.php';
+    exit;
 }
 
-// Enforce: must stay inside transcriptions/
-$prefix = $baseDir . DIRECTORY_SEPARATOR;
-if (strncmp($realFile, $prefix, strlen($prefix)) !== 0) {
-    http_response_code(404);
-    exit('Not found');
-}
-
-// Local-file read (not URL-capable sink)
-$fh = @fopen($realFile, 'rb');
-if ($fh === false) {
-    http_response_code(404);
-    exit('Not found');
-}
-
-$transcription = stream_get_contents($fh);
-fclose($fh);
+// Use local read
+$fh = fopen($file, 'rb');
+$transcription = $fh ? stream_get_contents($fh) : false;
+if ($fh) fclose($fh);
 
 if ($transcription === false) {
     http_response_code(404);
-    exit('Not found');
+    echo 'Not found';
+    require_once '../includes/htmlFooter.php';
+    exit;
 }
 
-//colour code the search term
-$q = Functions::getAccentInsensitive($q, false);
-$q = preg_quote($q, '/');
-$transcription = preg_replace("/({$q})/iu", '<span class="highlight">$0</span>', $transcription);
+// 4) Render page
+$q = $_GET['q'] ?? '';
+$qEsc = htmlspecialchars(is_string($q) ? $q : '', ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
 
-header('Content-Type: text/plain; charset=UTF-8');
-echo nl2br($transcription);
+echo <<<HTML
+  <div class="fixedNav">
+    <h4 class="transEx">{$qEsc}</h4>
+    <div class="controlBar">
+      <input id="findPrev" class="findTerm" value="prev" type="button"/>
+      <input id="findNext" class="findTerm" value="next" type="button"/>
+    </div>
+    <input type="button" id="transTop" value="back to top"/>
+  </div>
+
+  <div class="transcription">
+    <pre>
+HTML;
+
+echo htmlspecialchars($transcription, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+
+echo <<<HTML
+    </pre>
+  </div>
+HTML;
 
 require_once '../includes/htmlFooter.php';
