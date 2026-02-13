6b74a30eca222af931849c9878642d1ab37ffb60
steviebarrett <mail@steviebarrett.com>
2026-02-13 09:39:38 +0000

    and fix of Improper REstriction - permissive X-Frame-Layers in /LIL/includes/include.php

--- LIL/includes/include.php
+++ LIL/includes/include.php
@@ -82,7 +82,6 @@ if (empty($_SESSION['csrf_token'])) {
 // ========= Security headers =========
 if (!headers_sent()) {
     header('X-Content-Type-Options: nosniff');
-    header('X-Frame-Options: DENY');
     header('Referrer-Policy: strict-origin-when-cross-origin');
     header('Permissions-Policy: geolocation=(), microphone=(), camera=()');
 }
